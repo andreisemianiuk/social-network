d000495a12b522f6b8c503d716025c626387575e
andreisemianiuk <semeniukandrei677@gmail.com>
2021-05-15 23:03:51 +0300

    migrated from redux-form to formik

--- src/Components/Content/Profile/Posts/PostForm.tsx
+++ src/Components/Content/Profile/Posts/PostForm.tsx
@@ -1,31 +1,42 @@
 import React from 'react'
-import { Field, InjectedFormProps, reduxForm } from 'redux-form'
-import { addPostAC } from '../../../../redux/reducers/profile-reducer'
+import { ErrorMessage, Field, Form, Formik } from 'formik'
+import * as Yup from 'yup'
 import s from './Posts.module.css'
 
+export type PostFormPropsType = {
+  addPost: (value: string) => void
+}
 export type PostDataType = {
   post: string
 }
 
-const PostForm = (props: InjectedFormProps<PostDataType>) => {
+export const PostForm: React.FC<PostFormPropsType> = ({addPost}) => {
+  const initialValues: PostDataType = {post: ''}
   
   return (
-    <form onSubmit={props.handleSubmit}>
-      <div>
+    <Formik
+      initialValues={initialValues}
+      validationSchema={Yup.object({
+        post: Yup.string()
+          .max(30, 'Must be 30 characters or less')
+          .required('Required'),
+      })}
+      onSubmit={(values, actions) => {
+        addPost(values.post)
+        actions.setSubmitting(false)
+      }}
+    >
+      <Form>
         <Field
           name="post"
           component="textarea"
           className={s.textarea}
         />
-      </div>
-      <button className={s.btn}>Add Post</button>
-    </form>
+        <ErrorMessage name="post" component="div" className={s.error}/>
+        <div>
+          <button className={s.btn}>Add Post</button>
+        </div>
+      </Form>
+    </Formik>
   )
 }
-
-export const PostReduxForm = reduxForm<PostDataType>({
-  form: 'post',
-  // onSubmit: (values, dispatch) => {
-  //   dispatch(addPostAC(values.post))
-  // },
-})(PostForm)
--- src/Components/Content/Profile/Posts/Posts.tsx
+++ src/Components/Content/Profile/Posts/Posts.tsx
@@ -2,24 +2,16 @@ import React from 'react'
 import { Post } from './Post/Post'
 import { generateKey } from '../../../../utilities/keyCreator'
 import { PostsPropsType } from './PostsContainer'
-import { PostDataType, PostReduxForm } from './PostForm'
+import { PostForm } from './PostForm'
 import s from './Posts.module.css'
-import { AddPostForm } from './Post/PostFormWthFormik'
-import { addPostAC } from '../../../../redux/reducers/profile-reducer'
 
 export const Posts = (props: PostsPropsType) => {
-  const addPost = (values: PostDataType) => {
-    props.addPost(values.post)
-  }
-  const addPostOnKeyPress = (values: PostDataType) => {
-    props.addPost(values.post)
-  }
+  
   return (
     <div className={s.wrapper}>
       <h1>My Posts</h1>
-      <PostReduxForm onSubmit={addPost} />
-      {/*//component with formik*/}
-      {/*<AddPostForm/>*/}
+      <PostForm addPost={props.addPost}/>
+      
       {props.profilePage.posts.map(v => (
         <Post key={generateKey(v.id)}
               id={v.id}
