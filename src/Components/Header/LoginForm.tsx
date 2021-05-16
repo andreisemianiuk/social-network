526fd9d41c9b3f8003e86ae0a4c52a5f35ba60bd
andreisemianiuk <semeniukandrei677@gmail.com>
2021-05-16 07:38:02 +0300

    migrated from redux-form to formik

--- src/Components/Header/LoginForm.tsx
+++ src/Components/Header/LoginForm.tsx
@@ -1,9 +1,72 @@
 import React from 'react'
+import { Form, Formik } from 'formik'
+import { loginUserTC } from '../../redux/reducers/auth-reducer'
+import { useDispatch } from 'react-redux'
+import { MyCheckbox, MyTextInput } from '../../common/forms/inputsForForms'
 
-export const LoginForm = () => {
+export type FormDataType = {
+  email: string
+  password: string
+  rememberMe: boolean
+  captcha: boolean
+}
+
+const LoginForm: React.FC = () => {
+  const initialValues: FormDataType = {
+    email: '',
+    password: '',
+    rememberMe: false,
+    captcha: false,
+  }
+  const dispatch = useDispatch()
+  
+  return (
+    <Formik
+      initialValues={initialValues}
+      onSubmit={(values, actions) => {
+        dispatch(loginUserTC(values))
+        actions.setSubmitting(false)
+      }}
+    >
+      <Form>
+        <div>
+          <MyTextInput
+            label=""
+            name="email"
+            type="email"
+            placeholder="login"
+          />
+        </div>
+        <div>
+          <MyTextInput
+            label=""
+            name="password"
+            type="password"
+            placeholder="password"
+          />
+        </div>
+        <div>
+          <MyCheckbox
+            name="rememberMe"
+          >
+            remember me
+          </MyCheckbox>
+        </div>
+        <div>
+          <button type={'submit'}>Login</button>
+        </div>
+      </Form>
+    </Formik>
+  )
+}
+
+
+export const LoginPage = () => {
   return (
     <div>
-      Login
+      <h1>Login</h1>
+      <LoginForm/>
     </div>
   )
 }
+
