b32561f32ac3cd4a1859112a0cf0858b6a818442
andreisemianiuk <semeniukandrei677@gmail.com>
2021-05-16 21:05:21 +0300

    refactored login component for Header component

--- src/Components/Header/Login.tsx
+++ src/Components/Header/Login.tsx
@@ -1,32 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom'
-import { AuthStateType } from '../../redux/reducers/auth-reducer'
+import s from './Header.module.css'
 
 type LoginPropsType = {
-  data: AuthStateType
-  userData: Array<string | undefined>
+  isAuth: boolean
+  login: string | null
+  photo: string | undefined
+  logout: () => void
 }
 
 export const Login = (props: LoginPropsType) => {
-  // const {id, email, login} = props.data
-  const [name, img] = props.userData
+  const {login, photo, isAuth, logout} = props
+  const [isOpen, setIsOpen] = useState(false)
+  
+  const toggleMenu = () => {
+    setIsOpen(!isOpen)
+  }
+  // if (!isAuth) return <Redirect to={'login'}/>
   return (
     <div>
-      {
-        name
-          ?
-          <div>
-            <div>{name}</div>
-            <div>
-              <img src={img}/>
-            </div>
+      {isAuth
+        ?
+        <div className={s.userData}>
+          <div onClick={toggleMenu} className={s.photoContainer}>
+            <img className={s.photo} src={photo} alt={'ava'}/>
+            <div className={s.dropdown}></div>
+            {
+              isOpen ?
+                <div className={s.accountMenu}>
+                  <div className={s.menuList}>
+                    <p>{login}</p>
+                    <p>Settings</p>
+                    <hr/>
+                    <p className={s.logOut} onClick={logout}>Log Out</p>
+                  </div>
+                </div>
+                : null
+            }
           </div>
-          :
-          <div>
-            <NavLink to={'login'}>Sign In</NavLink>
-          </div>
-      }
-    
+        </div>
+        :
+        <div>
+          <NavLink to={'login'}>Log In</NavLink>
+        </div>}
     </div>
   )
 }
